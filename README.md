f060904e11870b1b6b178fc3388c8818375b93f1
Barry Carlyon <barry@barrycarlyon.co.uk>
2021-09-06 17:26:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,114 @@
+# insomna-twitch-extension-barrycarlyon
+
+This is a plugin for [insomnia](https://insomnia.rest) that will help with Twitch Extension Development by handle JWT Generation for you.
+
+## Configuration
+
+After installing this plugin. Create an Environment using this JSON Format:
+
+```json
+{
+  "client_id": "",
+  "client_secret": "",
+  "extension_secret": "",
+  "owner_id": "",
+  "version": ""
+}
+```
+
+Extension Secret should be the Base64 Encoded version, as the plugin will handle decoding.
+
+Don't forget to set this environment as active
+
+## Usage
+
+You will _not_ need to define any headers for the requests.
+This Plugin will add the `Client-ID` and `Authorization` headers for you
+
+You can also omit the following for each Endpoint this plugin supports
+
+
+### [Get Extension Configuration Segment](https://dev.twitch.tv/docs/api/reference#get-extension-configuration-segment)
+
+You do not need to add a `Query` parameter for `extension_id`. This plugin will do that for you.
+
+So just specify `segment` and `broadcaster_id` if needed
+
+### [Set Extension Configuration Segment](https://dev.twitch.tv/docs/api/reference#set-extension-configuration-segment)
+
+You do not need to add a `JSON Body` parameter for `extension_id`. This plugin will do that for you.
+
+The "content" of the Config Service needs to be JSON Encoded. But if you are "lazy" and specify an object, the plugin will auto JSON Encode the content for you.
+
+For example
+
+```json
+{
+    "segment": "global",
+    "content": {
+        "foo": "bar"
+    }
+}
+```
+
+or
+
+```json
+{
+    "segment": "global",
+    "content": "{\"foo\":\"bar\"}"
+}
+```
+
+Will work fine.
+
+### [Set Extension Required Configuration](https://dev.twitch.tv/docs/api/reference#set-extension-required-configuration)
+
+You do not need to add a `JSON Body` parameter for `extension_id` or `extension_version`. This plugin will do that for you.
+
+So a JSON body of
+
+```json
+{
+    "broadcaster_id": "xxxyyy",
+    "configuration_version": "zzzz"
+}
+```
+
+Will work fine.
+
+### [Send Extension PubSub Message](https://dev.twitch.tv/docs/api/reference#send-extension-pubsub-message)
+
+The "message" of the Send PubSub Message request needs to be JSON Encoded. But if you are "lazy" and specify an object, the plugin will auto JSON Encode the content for you.
+
+For example
+
+```json
+{
+    "segment": "global",
+    "content": {
+        "foo": "bar"
+    }
+}
+```
+
+or
+
+```json
+{
+    "segment": "global",
+    "content": "{\"foo\":\"bar\"}"
+}
+```
+
+### [Send Extension Chat Message](https://dev.twitch.tv/docs/api/reference#send-extension-chat-messagee)
+
+The plugin will set the required body parameters of `extension_id` and `extension_version` for you. So a JSON body of
+
+```json
+{
+    "text": "words"
+}
+```
+
+will do the job. Just don't forget the `Query` parameter of `broadcaster_id`
---
+++ plugin.js
@@ -0,0 +1,128 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.requestHooks = [
+    async context => {
+        console.log('Running insomna-twitch-extension-barrycarlyon')
+        const client_id = context.request.getEnvironmentVariable('client_id');
+        const client_secret = context.request.getEnvironmentVariable('client_secret');
+        const extension_secret = context.request.getEnvironmentVariable('extension_secret');
+        const owner_id = context.request.getEnvironmentVariable('owner_id');
+        const version = context.request.getEnvironmentVariable('version');
+
+        const extension_secret_usable = Buffer.from(extension_secret, 'base64');
+
+        let sigPayload = {
+            'exp':          Math.floor(new Date().getTime() / 1000) + 10,
+            'user_id':      owner_id,
+            'role':         'external'
+        }
+        let sig = jwt.sign(sigPayload, extension_secret_usable);
+
+        //console.log(context.request.getUrl());
+        let url = context.request.getUrl();
+
+        // configuration service
+        if (url.indexOf('extensions/configurations') >= 0) {
+            context.request.addHeader('Client-ID', `${client_id}`);
+            context.request.addHeader('Authorization', `Bearer ${sig}`);
+
+            if (context.request.getMethod() == 'PUT') {
+                let body = context.request.getBodyText();
+                try {
+                    body = JSON.parse(body);
+                    console.log(body);
+
+                    body.extension_id = (body.extension_id ? body.extension_id : client_id);
+
+                    // sanity check
+                    if (typeof body.content != 'string') {
+                        body.content = JSON.stringify(body.content);
+                    }
+
+                    console.log('Body now', body);
+
+                    context.request.setBodyText(JSON.stringify(body));
+                } catch (e) {
+
+                }
+            } else {
+                context.request.setParameter('extension_id', `${client_id}`);
+        }
+        }
+        if (url.indexOf('extensions/required_configuration') >= 0) {
+            context.request.addHeader('Client-ID', `${client_id}`);
+            context.request.addHeader('Authorization', `Bearer ${sig}`);
+
+            if (context.request.getMethod() == 'PUT') {
+                let body = context.request.getBodyText();
+                try {
+                    body = JSON.parse(body);
+                    console.log(body);
+
+                    body.extension_id = (body.extension_id ? body.extension_id : client_id);
+                    body.extension_version = (body.extension_version ? body.extension_version : version);
+
+                    console.log('Body now', body);
+
+                    context.request.setBodyText(JSON.stringify(body));
+                } catch (e) {
+
+                }
+            }
+        }
+
+        // pubsub
+        if (url.indexOf('extensions/pubsub') >= 0) {
+            let body = context.request.getBodyText();
+            try {
+                body = JSON.parse(body);
+                console.log(body);
+
+                let pubSubPayload = {
+                    'exp':          Math.floor(new Date().getTime() / 1000) + 10,
+                    'user_id':      owner_id,
+                    'role':         'external',
+                    'channel_id':   (body.target[0] == 'global' ? 'all' : body.target[0]),
+                    'pubsub_perms': {
+                        'send': body.target
+                    }
+                }
+                let pubSubSig = jwt.sign(pubSubPayload, extension_secret_usable);
+
+                context.request.addHeader('Client-ID', `${client_id}`);
+                context.request.addHeader('Authorization', `Bearer ${pubSubSig}`);
+
+                // sanity check
+                if (typeof body.message != 'string') {
+                    body.message = JSON.stringify(body.message);
+                }
+
+                console.log('Body now', body);
+
+                context.request.setBodyText(JSON.stringify(body));
+            } catch (e) {
+                // dont care
+                console.log('Extension Error', e);
+            }
+        }
+        // chat
+        if (url.indexOf('extensions/chat') >= 0) {
+            context.request.addHeader('Client-ID', `${client_id}`);
+            context.request.addHeader('Authorization', `Bearer ${sig}`);
+
+            let body = context.request.getBodyText();
+            try {
+                body = JSON.parse(body);
+                console.log(body);
+
+                body.extension_id = (body.extension_id ? body.extension_id : client_id);
+                body.extension_version = (body.extension_version ? body.extension_version : version);
+
+                context.request.setBodyText(JSON.stringify(body));
+            } catch (e) {
+                // dont care
+                console.log('Extension Error', e);
+            }
+        }
+    }
+]
